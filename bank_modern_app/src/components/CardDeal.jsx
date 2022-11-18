a8861eb7bf74acb70ba5ed7d87bfa3f19aa7fec1
sebastian <sebastiangs2309@gmail.com>
2022-11-18 00:38:53 -0500

    cardDeal done, need to fix the button and change the description

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { card } from "../assets"
+import styles, { layout } from '../style'
+import Button from './Button'
+
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section} >
+      <div className={layout.sectionInfo} >
+        <h2 className={styles.heading2} >need to chage this shit<br className='sm:block hidden' /> with my company blablab bla bla.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 `} >
+          FIND A GOOD DESCRIPTION ABOUT THE HOW THE USER CAN SE THEIR ACTIVITY   elit. Esse, non eaque deserunt tempore ad eum
+          repudiandae aliquid unde quis corporis labore iste, veritatis optio molestias maiores ex quae. Possimus, aliquid!
+        </p> 
+
+        <Button styles="mt-10" /> {/*I guess ill need to create another button component */}
+      </div>
+
+      <div className={layout.sectionImg} >
+        <img src={card} alt="card" className="w-[100%] h-[100%] "  />
+      </div>
+
+
+
+    </section>
   )
 }
 
