3c746854440d61461ae78bc99b42a02bbd613157
sebastian <sebastiangs2309@gmail.com>
2022-11-17 22:08:18 -0500

    billing section already done without google and apple

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { apple, bill, google } from "../assets"
+import styles, { layout } from '../style'
 
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <section id="product" className={layout.sectionImgReverse} >
+      <div className={layout.sectionImgReverse} >
+        <img src={bill} alt="billing" className="w-[100%] h-[100&] relative z-[5]  " />
+
+        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient  ' />
+        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient  ' />
+      </div>
+
+
+      <div className={layout.sectionInfo} >
+        <h2 className={styles.heading2} >
+          Paypal support <br className='sm:block hidden' />
+          the cryptocurrencies.  
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 `} >
+          Paypal is one of the many companies that started to accept payments in cryptocurrencies such as Mastercard, Starbucks, Norwegian, etc.
+        </p>
+      </div>
+
+    </section>
   )
 }
 
--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Button = ({ styles }) => {
   return (
     <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins 
