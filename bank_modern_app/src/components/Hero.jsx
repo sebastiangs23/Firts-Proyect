4fc2e0b3b2811067d24476a06bd05ad90118cb72
sebastian <sebastiangs2309@gmail.com>
2022-11-17 19:45:56 -0500

    business section and flow already done

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -5,8 +5,8 @@ import GetStarted from "./GetStarted"
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`} >
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 `} >
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} `} >
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`} >
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 " >
           <img src={discount} alt="discount" className='w-[32px] h-[32px]' />
           <p className={`${styles.paragraph}`} >
@@ -21,13 +21,13 @@ const Hero = () => {
 
 
         <div className='flex flex-row justify-between items-center w-full' >
-          <h1 className='flex-1 font-poppins font-semibold ss:text-[54px]
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[50px]
                        text-[52px] text-white ss:leading-[100px] ' >
             Best {" "}
             <span className='text-gradient'>
               Cryptocurrency <br className='sm:block hidden' /> {" "}
             </span> {" "}
-            software to connect with 
+            software to connect
           </h1>
 
           <div className='ss:flex hidden md:mr-4 mr-0' >
--- bank_modern_app/src/constants/index.js
+++ bank_modern_app/src/constants/index.js
@@ -74,17 +74,17 @@ export const stats = [
   {
     id: "stats-1",
     title: "User Active",
-    value: "3800+",
+    value: "7800+",
   },
   {
     id: "stats-2",
     title: "Trusted by Company",
-    value: "230+",
+    value: "55+",
   },
   {
     id: "stats-3",
-    title: "Transaction",
-    value: "$230M+",
+    title: "Trading Volume",
+    value: "$170M+",
   },
 ];
 
