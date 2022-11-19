79096adbf414a4929b949e3e109433a5b814066c
sebastian <sebastiangs2309@gmail.com>
2022-11-18 21:17:16 -0500

    change some shitty thing in redirect component

--- bank_modern_app/src/components/Redirect.jsx
+++ bank_modern_app/src/components/Redirect.jsx
@@ -7,7 +7,7 @@ const Redirect = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col `} >
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full `} >
-        <div className='flex-1 flex flex-col justify-start mr-10 ' >
+        <div className='flex-1 flex flex-col justify-start mr-0 ' >
           <img src={newlogo} alt="crypto" className='w-[266px] h-[72px] object-contain ' />
 
           <p className={`${styles.paragraph} mt-4 max-w-[310px] `} > All the official Links of the projects and protocols. </p>
@@ -16,12 +16,12 @@ const Redirect = () => {
         <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10 ' >
           {footerLinks.map((fl) => (
             <div key={fl.key} className='flex flex-col ss:my-0 my-4 min-y-[150px]'  >
-              <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white  '  >
+              <h4 className='font-poppins font-medium text-[38px] leading-[27px] text-white  '  >
                 {fl.title}
               </h4>
               <ul className='list-one mt-4' >
                 {fl.links.map((link, index) => (
-                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite 
+                  <li key={link.name} className={`font-poppins font-normal text-[26px] leading-[24px] text-dimWhite 
                                                   hover:text-secondary cursor-pointer ${index !== fl.length - 1 ? "mb-4" : 'mb-0'} `} >
                     {link.name}
                   </li>
--- bank_modern_app/src/components/Testimonials.jsx
+++ bank_modern_app/src/components/Testimonials.jsx
@@ -12,11 +12,11 @@ const Testimonials = () => {
 
       <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb16 mb:6 relative z-[1]' >
         <h1 className={styles.heading2} >
-          What the people are <br className='sm:block hidden' /> saying about us.
+          What the people are <br className='sm:block hidden' /> saying about the industry.
         </h1>
         <div className='w-full md:mt-0 mt-6'>
           <p className={`${styles.paragraph} text-left max-w-[450px] `} >
-            Everything you need to accept card payments and grow your business anywhere on the planet.
+            Everything blablablabl need to change this shitty thing.
           </p>
         </div>
       </div>
