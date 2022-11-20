1b055f784c935bfc0dd274389bb125d697af195f
sebastian <sebastiangs2309@gmail.com>
2022-11-20 18:58:44 -0500

    style crypto component only missed the button

--- bank_modern_app/src/components/Cryptos.jsx
+++ bank_modern_app/src/components/Cryptos.jsx
@@ -16,49 +16,61 @@ const Cryptos = () => {
 
     return (
         <div className='bg-primary w-full overflow-hidden'>
-            <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative `} >
-                <table class="shadow-2x1 font-[Poppins] border-2 border-cyan-200 w-6/12">
-                    <thead className='text-white'>
-                        <tr className='text-white'>
-                            <td>Rank</td>
-                            <td>Coin</td>
-                            <td>name</td>
-                            <td>symbol</td>
-                            <td>Price</td>
-                            <td>MarketCap</td>
+            <div className='absolute z-[0] w-[40%] h-[40%] right-550 bottom-20 top-50 pink__gradient' />
+            <div className='absolute z-[1] w-[40%] h-[50%] right-250 bottom-20 top-50 rounded-full bottom-40 pink__gradient ' />
 
-                            <td>Volumen</td>
-                            <td>Supply</td>
+            
+
+            <div className='h-[60px] w-[50px] bg-black-gradient' >
+                <Link to="/"  >
+                    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins 
+                          font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`} >
+                        Back
+                    </button>
+                </Link>
+            </div>
+            <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative `} >
+                <table class="shadow-2x1 font-[Poppins]  w-6/12">
+                    <thead className='text-white '>
+                        <tr className='text-white  hover:bg-cyan-100 hover:scale-105 flex flex-row p-6 rounded-[20px] feature-card'>
+                            <td className='py-8 px-4'>Rank</td>
+                            <td className='py-8 px-4'>Coin</td>
+                            <td className='py-8 px-4'>name</td>
+                            <td className='py-8 px-4'>symbol</td>
+                            <td className='py-8 px-4'>Price</td>
+                            <td className='py-8 px-4'>MarketCap</td>
+                            <td className='py-8 px-4'>Volumen</td>
+                            <td className='py-8 px-4'>Supply</td>
                         </tr>
 
                     </thead>
                     <tbody>
                         {saveCryptos.map((c) => {
                             return (
-                                <tr className='text-white'>
-                                    <td >
+                                <tr className='text-white sm:mr-5 mr-0  hover:bg-cyan-100 hover:scale-105 flex flex-row p-6 rounded-[20px]  feature-card'>
+                                    <td className='py-8 px-4'>
                                         {c.rank}
-                                    </td>
-                                    <td >
+                                    </td  >
+                                    <td className='py-8 px-4'>
                                         <img src={c.image} className="w-[28px] h-[27px]" />
                                     </td>
-                                    <td >
+                                    <td className='py-8 px-4'>
                                         {c.name}
                                     </td>
 
-                                    <td >
+                                    <td className='py-8 px-4'>
                                         {c.symbol}
                                     </td>
-                                    <td >
+                                    <td className='py-8 px-4'>
                                         {c.price}
                                     </td>
-                                    <td >
+                                    <td className='py-8 px-4'>
                                         {c.marketCap}
                                     </td>
-                                    <td >
+                                    <td className='py-8 px-4'>
                                         {c.volumen}
                                     </td>
-                                    <td >
+                                    <td className='py-8 px-4'>
                                         {c.supply}
                                     </td>
 
@@ -67,22 +79,15 @@ const Cryptos = () => {
                         }
                         )}
                     </tbody>
+                    
 
                 </table>
-                <div className='absolute z-[0] w-[20%] h-[20%] top-0 pink__gradient ' />
-                <div className='absolute z-[1] w-[30%] h-[30%] rounded-full bottom-40 pink__gradient ' />
+
                 <div className='absolute z-[0] w-[30%] h-[40%] right-20 bottom-20 blue__gradient ' />
             </div>
 
 
-            <div className='h-[40px] w-[30px] bg-black-gradient' >
-                <Link to="/"  >
-                    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins 
-                          font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`} >
-                        Back
-                    </button>
-                </Link>
-            </div>
+
 
         </div>
     )
