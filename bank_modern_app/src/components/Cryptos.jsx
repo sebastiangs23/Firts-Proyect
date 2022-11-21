0e9130a201698b17d127a486951a88a710a701e3
sebastian <sebastiangs2309@gmail.com>
2022-11-20 20:31:07 -0500

    crypto component done

--- bank_modern_app/src/components/Cryptos.jsx
+++ bank_modern_app/src/components/Cryptos.jsx
@@ -15,28 +15,29 @@ const Cryptos = () => {
     }, [])
 
     return (
-        <div className='bg-primary w-full overflow-hidden'>
-            <div className='absolute z-[0] w-[40%] h-[40%] right-550 bottom-20 top-50 pink__gradient' />
-            <div className='absolute z-[1] w-[40%] h-[50%] right-250 bottom-20 top-50 rounded-full bottom-40 pink__gradient ' />
+        <div className='bg-primary overflow-hidden  '> 
+            <div className='absolute z-[0] w-[25%] h-[10%] right-550 bottom-20 top-50 pink__gradient' />
+            <div className='absolute z-[1] w-[25%] h-[20%] right-250 bottom-20 top-50 rounded-full bottom-40 pink__gradient ' />
 
             
 
-            <div className='h-[60px] w-[50px] bg-black-gradient' >
-                <Link to="/"  >
+            <div class="flex justify-center items-center h-[112px] " >
+                <Link to="/" >
                     <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins 
                           font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`} >
                         Back
                     </button>
                 </Link>
             </div>
-            <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative `} >
+
+            <div  class="flex justify-center">
                 <table class="shadow-2x1 font-[Poppins]  w-6/12">
                     <thead className='text-white '>
-                        <tr className='text-white  hover:bg-cyan-100 hover:scale-105 flex flex-row p-6 rounded-[20px] feature-card'>
+                        <tr className='text-white  hover:bg-cyan-100 hover:scale-105 flex flex-row p-6 rounded-[20px] feature-card font-bold'>
                             <td className='py-8 px-4'>Rank</td>
                             <td className='py-8 px-4'>Coin</td>
-                            <td className='py-8 px-4'>name</td>
-                            <td className='py-8 px-4'>symbol</td>
+                            <td className='py-8 px-4'>Name</td>
+                            <td className='py-8 px-4'>Symbol</td>
                             <td className='py-8 px-4'>Price</td>
                             <td className='py-8 px-4'>MarketCap</td>
                             <td className='py-8 px-4'>Volumen</td>
@@ -48,17 +49,17 @@ const Cryptos = () => {
                         {saveCryptos.map((c) => {
                             return (
                                 <tr className='text-white sm:mr-5 mr-0  hover:bg-cyan-100 hover:scale-105 flex flex-row p-6 rounded-[20px]  feature-card'>
-                                    <td className='py-8 px-4'>
+                                    <td className='py-8 px-4 font-semibold'>
                                         {c.rank}
                                     </td  >
                                     <td className='py-8 px-4'>
                                         <img src={c.image} className="w-[28px] h-[27px]" />
                                     </td>
-                                    <td className='py-8 px-4'>
+                                    <td className='py-8 px-4 font-semibold'>
                                         {c.name}
                                     </td>
 
-                                    <td className='py-8 px-4'>
+                                    <td className='py-8 px-4 font-semibold'>
                                         {c.symbol}
                                     </td>
                                     <td className='py-8 px-4'>
